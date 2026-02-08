6c5d0d3b9212eb35f179c85a881800e3f7096dea
Agricog <mick@mickstevenson.com>
2026-02-08 15:58:10 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import Privacy from './pages/Privacy'
 import Terms from './pages/Terms'
-import './index.css'
 import MicroSaasPage from './pages/MicroSaasPage'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/micro-saas" element={<MicroSaasPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/micro-saas" element={<MicroSaasPage />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 )
-
