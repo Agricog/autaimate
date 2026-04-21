e1f0ae4fc6220617e0a7277c39003e0cd5848cc4
Agricog <mick@mickstevenson.com>
2026-04-21 18:18:48 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,21 @@ import App from './App'
 import Privacy from './pages/Privacy'
 import Terms from './pages/Terms'
 import MicroSaasPage from './pages/MicroSaasPage'
+
+// Product pages
 import TradeCalcsPage from './pages/products/TradeCalcsPage'
 import SnagLogPage from './pages/products/SnagLogPage'
 import CertVoicePage from './pages/products/CertVoicePage'
+import TradGoPage from './pages/products/TradGoPage'
+import DetailReconPage from './pages/products/DetailReconPage'
+import InspectVoicePage from './pages/products/InspectVoicePage'
+import SafeEatPage from './pages/products/SafeEatPage'
+
+// Industry hubs
 import ForConstructionPage from './pages/for/ConstructionPage'
+import ForPropertyPage from './pages/for/PropertyPage'
+import ForCareHomesPage from './pages/for/CareHomesPage'
+
 import './index.css'
 
 // Scrolls to top on route change, respects in-page anchors like /#contact
@@ -37,9 +48,15 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="/products/tradecalcs" element={<TradeCalcsPage />} />
           <Route path="/products/snaglog" element={<SnagLogPage />} />
           <Route path="/products/certvoice" element={<CertVoicePage />} />
+          <Route path="/products/tradgo" element={<TradGoPage />} />
+          <Route path="/products/detailrecon" element={<DetailReconPage />} />
+          <Route path="/products/inspectvoice" element={<InspectVoicePage />} />
+          <Route path="/products/safeeat" element={<SafeEatPage />} />
 
-          {/* Industry hubs */}
+          {/* Industry hubs (multi-product aggregators only) */}
           <Route path="/for/construction" element={<ForConstructionPage />} />
+          <Route path="/for/property" element={<ForPropertyPage />} />
+          <Route path="/for/care-homes" element={<ForCareHomesPage />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
